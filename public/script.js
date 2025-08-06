25427b645ce9d08f3094deff4a5b23ab7d57a590
JannDriceLorraineAbila <b2029jdlcabila@pshs.edu.ph>
2025-08-06 12:09:05 +0800

    commit abila

---
+++ public/script.js
@@ -0,0 +1,6 @@
+let x;
+    x = 100;
+    x = "Hi";
+    window.alert("Welcome to\n Web Development")
+    document.write("Welcome to <br/> Web Development")
+    console.log("Welcome to \n Web Development")
